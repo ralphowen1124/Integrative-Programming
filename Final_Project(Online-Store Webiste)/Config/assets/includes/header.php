75ce11679354d53addcbf1ec6586947b7734b585
Ralph Owen P. Castillo <owencastillo061@gmail.com>
2025-12-01 22:38:16 +0800

    Create header.php

---
+++ Final_Project(Online-Store Webiste)/Config/assets/includes/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CyberCart Store - Your Technology Destination</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+</head>
+<body>
+    <?php include 'includes/navbar.php'; ?>
+    
+    <main></main>
